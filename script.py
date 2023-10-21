85ee532ee81594cf150d8b8888735d14e30df695
Nathan Hendler <nathanhendler@gmail.com>
2023-10-21 16:44:09 -0700

    final version.  R.A. back to Dimitri.

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import threading
-from pynput import keyboard
 import sounddevice as sd
 import numpy as np
 import wavio
@@ -18,25 +17,10 @@ def list_audio_devices():
         print(f"  Default Sample Rate: {device['default_samplerate']}")
         print("---")
 
-def on_key_release(key, lock, skip_lock):
-    if key == keyboard.Key.space:
-        lock.set()
-    elif key == keyboard.KeyCode.from_char('s'):
-        skip_lock.set()
-
 def record_audio(filename, duration, samplerate, device=5):
     print()
     print(f"Ready to record: {filename}")
-    print("Press Space to start recording or 's' to skip...")
-
-    lock = threading.Event()
-    skip_lock = threading.Event()
-
-    with keyboard.Listener(on_release=lambda key: on_key_release(key, lock, skip_lock)) as listener:
-        lock.wait()
-        if skip_lock.is_set():
-            print("Skipping...")
-            return
+    input("Press Enter to start recording.")
 
     print(f"Recording {duration} seconds...")
     
@@ -52,22 +36,43 @@ def record_audio(filename, duration, samplerate, device=5):
     print(f"...saved.")
     print()
     
-    # Remove silence at the beginning
-    #audio_data = remove_silence(audio_data)
+    silenced_audio_data = remove_starting_silence(audio_data)
+
+    peak_value = np.max(np.abs(audio_data))
+    target_peak = 32767 * 10**(-3 / 20)
+
+    if peak_value < target_peak:
+        try:
+            normalization_factor = target_peak / peak_value
+            normalized_audio = audio_data * normalization_factor
+            wavio.write(f"recordings/normalized/{filename}_lowout.wav", silenced_audio_data[:, 0], samplerate, sampwidth=2)
+            wavio.write(f"recordings/normalized/{filename}_highout.wav", silenced_audio_data[:, 1], samplerate, sampwidth=2)
+        except:
+            pass
     
-    # Split channels and save
     left_channel = audio_data[:, 0]
     right_channel = audio_data[:, 1]
 
-    wavio.write(f"{filename}_lowout.wav", left_channel, samplerate, sampwidth=2)
-    wavio.write(f"{filename}_highout.wav", right_channel, samplerate, sampwidth=2)
-    
+    wavio.write(f"recordings/raw/{filename}_lowout.wav", left_channel, samplerate, sampwidth=2)
+    wavio.write(f"recordings/raw/{filename}_highout.wav", right_channel, samplerate, sampwidth=2)
+    try:
+        wavio.write(f"recordings/leading_silence_removed/{filename}_lowout.wav", silenced_audio_data[:, 0], samplerate, sampwidth=2)
+        wavio.write(f"recordings/leading_silence_removed/{filename}_highout.wav", silenced_audio_data[:, 1], samplerate, sampwidth=2)
+    except:
+        pass
     print(f"Saved {filename}_lowout.wav and {filename}_highout.wav")
+    return audio_data
 
-def remove_silence(audio_data, threshold=1000):
-    audio_abs = np.abs(audio_data)
-    silence_removed = audio_data[np.max(audio_abs, axis=1) > threshold]
-    return silence_removed
+#def remove_silence(audio_data, threshold=1000):
+    #audio_abs = np.abs(audio_data)
+    #silence_removed = audio_data[np.max(audio_abs, axis=1) > threshold]
+    #return silence_removed
+
+def remove_starting_silence(audio_data, silence_threshold=1000):
+    for i, sample in enumerate(audio_data):
+        if np.linalg.norm(sample) > silence_threshold:
+            return audio_data[i:]
+    return np.array([])  # return empty array if all samples are silent
 
 if __name__ == "__main__":
     # Ask for the BPM
@@ -78,20 +83,18 @@ if __name__ == "__main__":
     
     # Want to record for n loops
     n_loops = 10
+    #n_loops = 5
     beats = n_loops * 4
     minutes = 1/int(bpm) * beats
     duration = int(minutes * 60 + 1.5) # seconds
     samplerate = 44100  # Hertz
     
-    #patterns = ['Rock and Roll 1', 'Rock and Roll 2', 'Jazz', 'Blues']
-    patterns = top_row_patterns
-    
-    components = ['Cymbal', 'Guiro', 'Bass', 'Snare']
+    patterns = beat_patterns + latin_patterns + top_row_patterns
+    patterns = latin_patterns + top_row_patterns
+    #patterns = latin_patterns
     
     # Recording loop
     for pattern in patterns:
-        record_audio(f"{pattern}_{bpm}", duration, samplerate)
-        
-        #for component in components:
-            #record_audio(f"{pattern}_{component}_{bpm}", duration, samplerate)
-
+        #record_audio(f"{pattern}_{bpm}-noBass", duration, samplerate)
+        record_audio(f"{pattern}_{bpm}-noBass-noSnare-noCymbal-noGuiro", duration, samplerate)
+        #record_audio(f"{pattern}_{bpm}", duration, samplerate)
